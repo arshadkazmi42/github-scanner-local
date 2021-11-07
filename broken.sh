360f15547ee6018e12142f29fdde1dbab81f5a71
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-11-07 22:58:46 +0530

    Check broken with 100 parallel curl

--- broken.sh
+++ broken.sh
@@ -3,5 +3,5 @@ sh urls.sh ../github-scanner-local/results/$1.txt | tee -a ../github-scanner-loc
 
 cd ../github-scanner-local
 
-cat results/$1.txt | grep -v github.com | xargs -I {} sh ../bash-scripts/curl/scan-broken.sh {} | tee -a results/$1-broken.txt
+cat results/$1.txt | grep -v github.com | xargs -n1 -P100 -I {} sh ../bash-scripts/curl/scan-broken.sh {} | tee -a results/$1-broken.txt
 
