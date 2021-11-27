97d1a122d09ea579015ab3ee34348c68e039fe3b
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-11-27 21:47:26 +0530

    Ruby improvemnt

--- getRubyDeps.sh
+++ getRubyDeps.sh
@@ -1,4 +1,4 @@
 mkdir -p dependencies/ruby/
 
-find $1 -name Gemfile | xargs -I {} awk '{print}' {} | grep -v "\#" | grep -v rubygems.org | grep -v gemspec | grep gem | sed "s/\"/\'/g" | awk -F "\'" '{print $2}' | awk NF | sort | uniq | tee -a dependencies/ruby/$1.txt
+find $1 -name Gemfile | xargs -I {} awk '{print}' {} | grep "^gem" | grep -v gemspec | sed "s/\"/\'/g" | awk -F "\'" '{print $2}' | awk NF | sort | uniq | tee -a dependencies/ruby/$1.txt
 
