3e9d7a547f6148a6b3608967df3e8130658c8eef
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-11-27 19:30:59 +0530

    Improvement to pip dependency checker

---
+++ pip-name.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+HTTP_CODE=404
+
+response_code=$(curl --max-time 5 -Ls -o /dev/null -w "%{http_code}" https://pypi.org/project/$1)
+
+if [ $response_code -eq $HTTP_CODE ]; then
+    echo "${GREEN}"$1"${NC} is available"
+else
+    echo "${RED}"$1"${NC} is unavailable"
+fi
