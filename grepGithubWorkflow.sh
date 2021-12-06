637fe0677e452fc1dadb9f932e7daa78422364a6
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-12-06 12:50:54 +0530

    improvements

--- grepGithubWorkflow.sh
+++ grepGithubWorkflow.sh
@@ -2,4 +2,4 @@ mkdir -p results-github-workflow
 
 echo "Searching for github accounts from workflows"
 
-grep -roh -E "uses: [-a-zA-Z0-9\.]+/[-a-zA-Z0-9.]+\@[-a-zA-Z0-9\.]+" $1 | awk -F ": " '{print $2}' | awk -F "/" '{print "https://github.com/"$1}' | sort | uniq | tee -a results-github-workflow/$1.txt
+grep -roh -E "uses: [-a-zA-Z0-9\.]+/[-a-zA-Z0-9.]+\@[-a-zA-Z0-9\.]+" $1 | awk -F ": " '{print $2}' | awk -F "/" '{print "https://github.com/"$1}' | sort | uniq | grep -v "^actions$" | tee -a results-github-workflow/$1.txt
