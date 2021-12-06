8d9bc115622aad89fd27ac7f8c06a9d21cbdc5b2
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-12-06 11:44:40 +0530

    improvemens

--- getNpmRegistryDeps.sh
+++ getNpmRegistryDeps.sh
@@ -1,1 +1,3 @@
-cat results/$1-broken.txt | grep "https://registry.npmjs.org" | awk -F " " '{print $2}' | awk -F "/" '{print $4}' | sort | uniq
+mkdir -p dependencies/npm-registry/
+
+cat results/$1-broken.txt | grep "https://registry.npmjs.org" | awk -F " " '{print $2}' | awk -F "/" '{print $4}' | sort | uniq | tee -a dependencies/npm-registry/$1.txt
