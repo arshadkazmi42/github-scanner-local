142845c2323a92859f0f0853d98391705bb4f2a1
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-12-15 22:17:31 +0530

    Improve scripts runBuckets

--- runBuckets.sh
+++ runBuckets.sh
@@ -1,1 +1,1 @@
-grep -roh -E "(s3|gs)://[-a-zA-Z0-9_\.]+" $1 | grep -vi "example\|bucketname$\|bucket-name$\|BUCKET_NAME$\|mybucket$\|my_bucket$" | sort | uniq
+grep -ro -E "(s3|gs)://[-a-zA-Z0-9_\.]+" $1 | grep -vi "example\|bucketname$\|bucket-name$\|BUCKET_NAME$\|mybucket$\|my_bucket$" | sort | uniq
