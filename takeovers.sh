f3032eb8950378ed295164fca5dbe37c1d9991c0
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-12-21 11:27:51 +0530

    Improve takeover script

--- takeovers.sh
+++ takeovers.sh
@@ -1,1 +1,1 @@
-cat results/$1-broken.txt | grep "googleapis\|amazonaws\|vercel.app\|netlify\|herokuapp\|surge.sh\|now.sh\|opencollective\|pattreon\|plugins.svn.wordpress.org"
+cat results/$1-broken.txt | grep "googleapis\|amazonaws\|vercel.app\|netlify\|herokuapp\|surge.sh\|now.sh\|opencollective\|pattreon\|plugins.svn.wordpress.org\|npmjs.org\/package"
