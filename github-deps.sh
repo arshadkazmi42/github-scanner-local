28a4e986f11110926512c8e7da5f145c23bc071b
Arshad Kazmi <arshadkazmi42@gmail.com>
2022-03-05 22:37:46 +0530

    Sciprts update

--- github-deps.sh
+++ github-deps.sh
@@ -4,7 +4,7 @@ mkdir -p $GITHUB_PATH
 
 grep -r -oP 'github:[-a-zA-Z0-9\/\.\_]+' $1 | tee -a $GITHUB_PATH/$1.txt 
 
-cat $GITHUB_PATH/$1.txt | awk -F ":" '{print $3}' | awk -F "/" '{print "https://github.com/"$1}' | sort | uniq | tee -a $GITHUB_PATH/$1-links.txt
+cat $GITHUB_PATH/$1.txt | awk -F ":" '{print $3}' | awk -F "/" '{print "https://github.com/"$1}' | grep -v "github.com/before" | sort | uniq | tee -a $GITHUB_PATH/$1-links.txt
 
 cd ../is-broken-link-github/
 
